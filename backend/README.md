330f4c2582d7f079dc7f1dc5cbb702341098e215
WataminC <WataminC@outlook.com>
2024-12-18 19:47:41 +0800

    BackEnd: Initial gin engine successfully

---
+++ backend/README.md
@@ -0,0 +1,5 @@
+Development:
+
+1. Read yaml configuration file
+2. Define models
+3. Connect database and auto migrate
--- backend/main.go
+++ backend/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"bank/config"
+	"bank/router"
 	"fmt"
 )
 
 func main() {
 	config.InitConfig()
 	fmt.Print("Configuration successful\n")
+
+	r := router.SetRouter()
+	r.Run()
 }
